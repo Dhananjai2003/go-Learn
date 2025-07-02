0d79f01ad4095f2c1f013986db3f11797bc8861d
Dhananjai2003 <dhananjaimurali30@gmail.com>
2025-07-02 19:04:22 +0530

    completed url shortner and test.sh is there for curl api calls

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST -H "Content-Type: application/json" -d '{"url":"https://myspace.com"}' http://localhost:8080/short
+curl -X POST -H "Content-Type: application/json" -d '{"url":"https://chatgpt.com/c/68538c19-6278-800e-ac98-1368119adbbc"}' http://localhost:8080/short
